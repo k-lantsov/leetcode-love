60d62d611af75597429f23cf0419c5195ecdd4b1
k.lantsov <k.lantsov@yahoo.com>
2024-06-08 23:13:00 +0300

    remove duplicates

---
+++ src/test/java/com/example/RemoveDuplicatesTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RemoveDuplicatesTest {
+
+    @Test
+    void removeDuplicatesExample1() {
+        assertThat(RemoveDuplicates.removeDuplicates(new int[]{1, 1, 2})).isEqualTo(2);
+    }
+
+    @Test
+    void removeDuplicatesExample2() {
+        assertThat(RemoveDuplicates.removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4})).isEqualTo(5);
+    }
+
+    @Test
+    void removeDuplicatesExample3() {
+        assertThat(RemoveDuplicates.removeDuplicates(new int[]{})).isEqualTo(0);
+    }
+
+    @Test
+    void removeDuplicatesExample4() {
+        assertThat(RemoveDuplicates.removeDuplicates(new int[]{1, 1, 1, 1})).isEqualTo(1);
+    }
+}
