14d0d4ca7c2c7c6a0f62d1ca1387761c0ae5c7c5
k.lantsov <k.lantsov@yahoo.com>
2024-06-14 22:38:09 +0300

    greatest common divisor of strings

---
+++ src/test/java/com/example/leetcode75/GreatestCommonDivisorOfStringsTest.java
@@ -0,0 +1,38 @@
+package com.example.leetcode75;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GreatestCommonDivisorOfStringsTest {
+
+    @Test
+    void gcdOfStringsExample1() {
+        assertThat(GreatestCommonDivisorOfStrings.gcdOfStrings("ABCABC", "ABC")).isEqualTo("ABC");
+    }
+
+    @Test
+    void gcdOfStringsExample2() {
+        assertThat(GreatestCommonDivisorOfStrings.gcdOfStrings("ABABAB", "AB")).isEqualTo("AB");
+    }
+
+    @Test
+    void gcdOfStringsExample3() {
+        assertThat(GreatestCommonDivisorOfStrings.gcdOfStrings("LEET", "CODE")).isEqualTo("");
+    }
+
+    @Test
+    void gcdOfStringsExample4() {
+        assertThat(GreatestCommonDivisorOfStrings.gcdOfStrings("AB", "AB")).isEqualTo("AB");
+    }
+
+    @Test
+    void gcdOfStringsExample5() {
+        assertThat(GreatestCommonDivisorOfStrings.gcdOfStrings("AAAAA", "AA")).isEqualTo("A");
+    }
+
+    @Test
+    void gcdOfStringsExample6() {
+        assertThat(GreatestCommonDivisorOfStrings.gcdOfStrings("AAAAAA", "AA")).isEqualTo("AA");
+    }
+}
