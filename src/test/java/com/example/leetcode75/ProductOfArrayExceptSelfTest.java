75176f1df1a9cf783c971722f7f43f23d8f9daa6
k.lantsov <k.lantsov@yahoo.com>
2024-06-16 23:26:50 +0300

    product of array except self

---
+++ src/test/java/com/example/leetcode75/ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,18 @@
+package com.example.leetcode75;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductOfArrayExceptSelfTest {
+
+    @Test
+    void productExceptSelfExample1() {
+        assertThat(ProductOfArrayExceptSelf.productExceptSelf(new int[]{1, 2, 3, 4})).isEqualTo(new int[]{24, 12, 8, 6});
+    }
+
+    @Test
+    void productExceptSelfExample2() {
+        assertThat(ProductOfArrayExceptSelf.productExceptSelf(new int[]{-1, 1, 0, -3, 3})).isEqualTo(new int[]{0, 0, 9, 0, 0});
+    }
+}
