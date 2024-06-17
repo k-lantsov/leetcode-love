f58507d52389cad70f6cd9ae47540b2b94a8f69f
k.lantsov <k.lantsov@yahoo.com>
2024-06-18 01:09:14 +0300

    is subsequence

---
+++ src/main/java/com/example/leetcode75/IsSubsequence.java
@@ -0,0 +1,18 @@
+package com.example.leetcode75;
+
+public class IsSubsequence {
+
+    public static boolean isSubsequence(String s, String t) {
+        int sp = 0;
+        int tp = 0;
+        int sLength = s.length();
+        int tLength = t.length();
+        while (sp < sLength && tp < tLength){
+            if (s.charAt(sp) == t.charAt(tp)) {
+                sp++;
+            }
+            tp++;
+        }
+        return sp == sLength;
+    }
+}
---
+++ src/test/java/com/example/leetcode75/IsSubsequenceTest.java
@@ -0,0 +1,33 @@
+package com.example.leetcode75;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IsSubsequenceTest {
+
+    @Test
+    void isSubsequenceExample1() {
+        assertThat(IsSubsequence.isSubsequence("abc", "ahbgdc")).isEqualTo(true);
+    }
+
+    @Test
+    void isSubsequenceExample2() {
+        assertThat(IsSubsequence.isSubsequence("axc", "ahbgdc")).isEqualTo(false);
+    }
+
+    @Test
+    void isSubsequenceExample3() {
+        assertThat(IsSubsequence.isSubsequence("", "")).isEqualTo(true);
+    }
+
+    @Test
+    void isSubsequenceExample4() {
+        assertThat(IsSubsequence.isSubsequence("", "abc")).isEqualTo(true);
+    }
+
+    @Test
+    void isSubsequenceExample5() {
+        assertThat(IsSubsequence.isSubsequence("abc", "")).isEqualTo(false);
+    }
+}
